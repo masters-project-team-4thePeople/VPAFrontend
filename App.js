5ae026637e2d97ccf03de7bf5bf1a9bd959a91a1
Swathi04 <swathianandram@gmail.com>
2022-11-11 11:17:54 -0800

    UI changes

---
+++ App.js
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+import Home from './screens/Home';
+import Login from './screens/Login';
+import Details from './screens/Details';
+import SignUp from './screens/SignUp';
+import ResetPassword from './screens/ResetPassword';
+
+const Stack = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: "white"
+  }
+}
+
+const App = () => {
+
+  const [loaded] = useFonts({
+    InterBold: require("./assets/fonts/Inter-Bold.ttf"),
+    InterSemiBold: require("./assets/fonts/Inter-SemiBold.ttf"),
+    InterMedium: require("./assets/fonts/Inter-Medium.ttf"),
+    InterRegular: require("./assets/fonts/Inter-Regular.ttf"),
+    InterLight: require("./assets/fonts/Inter-Light.ttf"),
+  })
+
+  if(!loaded) return null;
+
+  return (
+    <NavigationContainer theme={theme}>
+      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Login">
+        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name='SignUp' component={SignUp}/>
+        <Stack.Screen name='Home' component={Home}/>
+        <Stack.Screen name='Details' component={Details}/>
+        <Stack.Screen name='ResetPassword' component={ResetPassword}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
